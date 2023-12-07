c76d4714c5d91f5520eae24b7a0264d16f55bf9c
Stefan Popovski <stefan.sheva.popovski@gmail.com>
2023-12-07 20:42:09 +0100

    Solution for day4 part 1

---
+++ day4/day4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+const string CardsSeparator = "\n";
+const string CardNumberSeparator = "|";
+const string NormalizationRegex = @"(Card \d+:)";
+Console.WriteLine("Day 4, Part One START");
+
+var gameInput = @"
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+gameInput = await File.ReadAllTextAsync("./input.txt");
+var normalizeInput = Regex.Replace(gameInput, NormalizationRegex, string.Empty);
+var cards = normalizeInput.Trim().Split(CardsSeparator);
+
+var winningNumbersCounter = 0.0;
+const int expBase = 2;
+foreach (var card in cards)
+{
+    var groupedCards = card.Split(CardNumberSeparator);
+    var winningNumbers = groupedCards
+        .First()
+        .Split(" ")
+        .Where(x => !string.IsNullOrWhiteSpace(x));
+    var pickedNumbers = groupedCards
+        .Last()
+        .Split(" ")
+        .Where(x => !string.IsNullOrWhiteSpace(x))
+        .Count(num => winningNumbers.Contains(num.Trim()));
+
+    winningNumbersCounter += pickedNumbers > 0 ? Math.Pow(expBase, pickedNumbers - 1) : 0;
+}
+
+Console.WriteLine($"Total Points: {winningNumbersCounter}");
+Console.WriteLine("Day 4, Part One END");
